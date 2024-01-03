a141875f38298b62b27c82c57e9d664ac402daf7
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2024-01-03 11:12:12 -0500

    ES6 Basics

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (const idx in array) {
-    const value = array[idx];
-    array[idx] = appendString + value;
+  const newArray = [];
+  for (const value of array) {
+    newArray.push(appendString + value);
   }
 
-  return array;
+  return newArray;
 }
