d0f828c6777eee821c05771bdc710e7480bdcabd
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2024-01-04 08:53:44 -0500

    fixed eslint

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,7 +1,7 @@
 import ClassRoom from './0-classroom';
 
-export default function initializeRoom () {
-	const array = []
-	array.push(new ClassRoom(19), new ClassRoom(20), new ClassRoom(34));
-	return array;
-};
+export default function initializeRoom() {
+  const array = [];
+  array.push(new ClassRoom(19), new ClassRoom(20), new ClassRoom(34));
+  return array;
+}
