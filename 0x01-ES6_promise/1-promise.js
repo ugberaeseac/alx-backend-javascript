61edfca75019f15a35b2e689cae8ca7cb8aa7ced
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2024-01-05 17:45:01 -0500

    ES6 Promise

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,5 +1,5 @@
 export default function getFullResponseFromAPI(success) {
- return new Promise(((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     if (success === true) {
       resolve({
         status: 200,
@@ -8,5 +8,5 @@ export default function getFullResponseFromAPI(success) {
     } else {
       reject(new Error('The fake API is not working currently'));
     }
-  }));
+  });
 }
