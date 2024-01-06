aee490ce422142a236ccd5215b45bb20e6d79851
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2024-01-06 15:36:10 -0500

    ES6 Promise

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto, createUser])
+  return Promise.all([uploadPhoto(), createUser()])
     .then((response) => {
       const { body } = response[0];
       const { firstName, lastName } = response[1];
--- 0x01-ES6_promise/utils.js
+++ 0x01-ES6_promise/utils.js
@@ -1,11 +1,11 @@
-export default function uploadPhoto() {
+export function uploadPhoto() {
 	return Promise.resolve({
 		  status: 200,
 		  body: 'photo-profile-1',
 	});
 }
 
-export default function createUser() {
+export function createUser() {
 	return Promise.resolve({
 		  firstName: 'Guillaume',
 		  lastName: 'Salva',
