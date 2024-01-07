bdabb63c1bb9dc5385150050c55c42cad93a5ec6
Ugberaese Charles A <ugberaeseac@gmail.com>
2024-01-07 14:31:16 -0800

    ES6 Basics

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,3 +1,19 @@
 export default function createIteratorObject(report) {
-  return
+	let iterable = [];
+
+	if (!report.allEmployees || typeof report.allEmployees !== 'object') {
+		return iterable;
+	}
+
+	iterable = {
+		* [Symbol.iterator]() {
+			for (const value of Object.values(report.allEmployees)) {
+				for (const i of value) {
+					yield i;
+				}
+			}
+		},
+	};
+
+	return iterable;
 }
