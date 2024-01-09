496c524436979e8516ab497fb288ea3c74d03238
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2024-01-09 03:46:37 -0500

    ES6 Data manipulations - Filter/Map Combine

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,15 +1,15 @@
 export default function updateStudentGradeByCity(array, city, newGrades) {
-    const newArray = array.filter((student) => student.location === city);
-    const gradedStudents = newArray.map((student) => {
-      const filterGrade = newGrades.filter((studentGrade) => studentGrade.studentId === student.id);
+  const newArray = array.filter((student) => student.location === city);
+  const gradedStudents = newArray.map((student) => {
+    const filterGrade = newGrades.filter((studentGrade) => studentGrade.studentId === student.id);
 
-      let grades;
-      if (filterGrade[0]) {
-        grades = filterGrade[0].grade;
-      } else {
-        grades = 'N/A';
-      }
-      return { ...student, grades };
-    });
-    return gradedStudents;
-  }
+    let grades;
+    if (filterGrade[0]) {
+      grades = filterGrade[0].grade;
+    } else {
+      grades = 'N/A';
+    }
+    return { ...student, grades };
+  });
+  return gradedStudents;
+}
