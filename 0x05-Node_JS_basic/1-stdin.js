e37b6c2222afff55199ed0202f1adb67735abd40
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2024-02-27 18:40:32 -0500

    Reading a file synchronously with Node JS

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,5 @@
+const process = require('node:process');
+
+process.stdout.on('write' () => {};
+
+process.stdout.on('exit' () => {};
---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+
+const countStudent = (path) => {
+  try {
+    let data = fs.readFileSync(path, 'utf8').toString().split('\n');
+    data = data.slice(1, data.length - 1);
+    console.log(`Number of students: ${data.length}`);
+    const subjects = {};
+    for (const row of data) {
+      const student = row.split(',');
+      if (!subjects[student[3]]) {
+        subjects[student[3]] = [];
+      }
+      subjects[student[3]].push(student[0]);
+    }
+    for (const subject of subjects) {
+      console.log(subject);
+      console.log(`Number of students in ${subject} : ${subjects[subject].length}. List: ${subjects[subject].join(', ')}`);
+    }
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+};
+
+module.exports = countStudent;
