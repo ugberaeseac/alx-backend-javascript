fde7b387975a966336821c33fb81cbd2d37d5b24
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2024-03-01 17:58:43 -0500

    organising a complex HTTP request server using Express - adding files

---
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+
+const readDatabase = (path) => new Promise((resolve, reject) => {
+  fs.readFile(path, 'utf8', (err, data) => {
+    if (err) {
+      reject(new Error(err));
+    };
+  };
+};
