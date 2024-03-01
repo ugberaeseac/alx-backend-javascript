8c4991677cb3718d9d2bf6a24aae663d1ea9ca21
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2024-03-01 17:32:19 -0500

    debug

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -4,21 +4,17 @@ const countStudents = require('./3-read_file_async');
 const app = http.createServer(async (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-
-  if (req.url === '/') {
-    res.write('Hello Holberton School!');
-  }
+  if (req.url === '/') res.write('Hello Holberton School!');
   if (req.url === '/students') {
     res.write('This is the list of our students\n');
     try {
-      const students = await countStudents(process.argv[2]);
-      res.end(`${students.join('\n')}`);
+      const data = await countStudents(process.argv[2]);
+      res.end(`${data.join('\n')}`);
     } catch (error) {
       res.end(error.message);
     }
   }
   res.end();
 });
-
 app.listen(1245);
 module.exports = app;
